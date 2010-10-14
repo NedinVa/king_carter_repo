4d9017b90860463bc105abcf21fb1365a173330e
Steve Majewski <sdm7g@virginia.edu>
2010-10-14 19:14:45 +0000

    convert some links from abs to rel

--- etcbin/run-carter.sh
+++ etcbin/run-carter.sh
@@ -6,3 +6,9 @@ echo "writing html/${BASE}.html..."
 perl etcbin/browse-carter  | tr '\r' '\n' | tail +3 >html/${BASE}.html 
 echo "writing html/${BASE}.mod.html..."
 perl etcbin/browse-cartermod | tr '\r' '\n' | tail +3 >html/${BASE}.mod.html
+#
+# browse-carter & browser-cartermod are now the same file
+# browse-cartermod is a symbolic link to browse-carter.
+# The logic is the same except it then chains to either carter.pl or cartermod.pl,
+# depending on how it was invoked. 
+#
