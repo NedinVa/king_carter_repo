4bbcaa3b6a1fed1ad4197e4b11232d4c3e0a38f1
Steve Majewski <sdm7g@virginia.edu>
2010-10-18 17:46:06 +0000

    regenerate static files

--- etcbin/run-carter.sh
+++ etcbin/run-carter.sh
@@ -2,14 +2,19 @@
 # this script should be run from it's parent directory 
 # which is the top directory of berkeley prj web site.
 #
-FILE=$(basename $1)
-BASE=$(basename $1 .xml)
-export QUERY_STRING="id=${FILE}&images=XYZZY&data=.&tag=public"
-echo $QUERY_STRING
-echo "writing html/${BASE}.html..."
-perl etcbin/browse-carter  | etcbin/abs2relhref.pl | tr '\r' '\n' | tail +3 >html/${BASE}.html 
-echo "writing html/${BASE}.mod.html..."
-perl etcbin/browse-cartermod | etcbin/abs2relhref.pl | tr '\r' '\n' | tail +3 >html/${BASE}.mod.html
+if [ -n "$1" -a -e "$1" ]
+then
+ FILE=$(basename $1)
+ BASE=$(basename $1 .xml)
+ export QUERY_STRING="id=${FILE}&images=XYZZY&data=.&tag=public"
+ echo $QUERY_STRING
+ echo "writing html/${BASE}.html..."
+ perl etcbin/browse-carter  | etcbin/abs2relhref.pl | tr '\r' '\n' | tail +3 >html/${BASE}.html 
+ echo "writing html/${BASE}.mod.html..."
+ perl etcbin/browse-cartermod | etcbin/abs2relhref.pl | tr '\r' '\n' | tail +3 >html/${BASE}.mod.html
+else
+ echo "nothing?" 
+fi
 #
 # 
 # browse-carter & browser-cartermod are now the same file
