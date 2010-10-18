a1cb48249ace8e5e3f79174a709d7dc2ac0409ec
Steve Majewski <sdm7g@virginia.edu>
2010-10-18 17:21:35 +0000

    convert abs --> rel href links

---
+++ etcbin/abs2relhref.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+while ( <> ) {
+
+ s/href="http:\/\/etext.lib.virginia.edu\/users\/berkeley\/public\/Cbiodir.html#[^"]*"/href="Cbiodir.html#$1"/g ;
+ s/href="http:\/\/etext.lib.virginia.edu\/users\/berkeley\/public\/Cbibl.html#[^"]*"/href="Cbibl.html#$1"/g ;
+
+ s/href="http:\/\/etext.lib.virginia.edu\/etcbin\/browse-carter?id=(C[0-9a-zA-Z.]+).xml[^"]*"/href="$1.html"/g ;
+
+ print ;
+
+}
--- etcbin/run-carter.sh
+++ etcbin/run-carter.sh
@@ -1,12 +1,17 @@
+#
+# this script should be run from it's parent directory 
+# which is the top directory of berkeley prj web site.
+#
 FILE=$(basename $1)
 BASE=$(basename $1 .xml)
 export QUERY_STRING="id=${FILE}&images=XYZZY&data=.&tag=public"
 echo $QUERY_STRING
 echo "writing html/${BASE}.html..."
-perl etcbin/browse-carter  | tr '\r' '\n' | tail +3 >html/${BASE}.html 
+perl etcbin/browse-carter  | etcbin/abs2relhref.pl | tr '\r' '\n' | tail +3 >html/${BASE}.html 
 echo "writing html/${BASE}.mod.html..."
-perl etcbin/browse-cartermod | tr '\r' '\n' | tail +3 >html/${BASE}.mod.html
+perl etcbin/browse-cartermod | etcbin/abs2relhref.pl | tr '\r' '\n' | tail +3 >html/${BASE}.mod.html
 #
+# 
 # browse-carter & browser-cartermod are now the same file
 # browse-cartermod is a symbolic link to browse-carter.
 # The logic is the same except it then chains to either carter.pl or cartermod.pl,
