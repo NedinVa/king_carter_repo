6c53bc600c22a5cf35e9ddc3ea6e6ccf83bd478f
Steve Majewski <sdm7g@virginia.edu>
2010-10-19 21:29:34 +0000

    more replacements

--- etcbin/abs2relhref.pl
+++ etcbin/abs2relhref.pl
@@ -6,6 +6,10 @@ while ( <> ) {
  s/href="http:\/\/etext.lib.virginia.edu\/users\/berkeley\/public\/Cbibl.html#([^"]*)"/href="Cbibl.html#$1"/g ;
 
  s/href="http:\/\/etext.lib.virginia.edu\/etcbin\/browse-carter?id=(C[0-9a-zA-Z.]+).xml[^"]*"/href="$1.html"/g ;
+ 
+ s/\/users\/berkeley\/public\/([^.]+)\.xml/$1.html/g ; 
+ 
+ s/\/users\/berkeley\/public\///g ; 
 
  s/<!-- .* -->/ /g ;  # remove commented out teiHeader
  
