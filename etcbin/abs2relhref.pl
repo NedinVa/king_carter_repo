cafa78a21d596d2b0134832fbb3b05b5ec4c92de
Steve Majewski <sdm7g@virginia.edu>
2010-10-22 16:49:07 +0000

    fix regex typo and regenerate html

--- etcbin/abs2relhref.pl
+++ etcbin/abs2relhref.pl
@@ -7,7 +7,9 @@ while ( <> ) {
  s/href="http:\/\/etext.lib.virginia.edu\/users\/berkeley\/public\/Cbiodir.html#([^"]*)"/href="Cbiodir.html#$1"/g ;
  s/href="http:\/\/etext.lib.virginia.edu\/users\/berkeley\/public\/Cbibl.html#([^"]*)"/href="Cbibl.html#$1"/g ;
 
- s/href="http:\/\/etext.lib.virginia.edu\/etcbin\/browse-carter?id=(C[0-9a-zA-Z.]+).xml[^"]*"/href="$1.html"/g ;
+ s/href="http:\/\/etext.lib.virginia.edu\/etcbin\/browse-carter\?id=(C[0-9a-zA-Z.]+).xml[^"]*"/href="$1.html"/g ;
+ 
+# s/href="http:\/\/etext.lib.virginia.edu\/etcbin\/browse-carter?id=(http:\/\/[^&]+)&?[^"]*"/href="$1"/g ;
  
  s/\/users\/berkeley\/public\/([^.]+)\.xml/$1.html/g ; 
  
